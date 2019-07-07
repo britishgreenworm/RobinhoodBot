080927f61358825753c4f7caed6a81c0d6a5b1eb
Dano <britishgreenworm@gmail.com>
2019-07-07 17:37:38 -0400

    Added core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobinhoodBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- RobinhoodBot.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public class RobinhoodBot{
-    public static void main(){
-        
-    }
-}
